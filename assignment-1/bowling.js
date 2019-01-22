840ce4aaecee32f21aea1b9aa98b6d2c1dcb19d7
Pulkit Agarwal <pulkit_agarwal-bng@external.mckinsey.com>
2019-01-22 10:58:01 +0530

    changed wrong naming convention of strike and added strike,spare both

--- assignment-1/bowling.js
+++ assignment-1/bowling.js
@@ -3,18 +3,29 @@ const rolls = (...arg)=>{
 	console.log(strike(arg,0));
 	return [...arg];
 };
+const spare = (spare_array,spare_index)=>{
+	if((spare_array.length-spare_index)<3)
+	{
+		return new Error('less number of arguments');
+	}
+	if(spare_array[spare_index]+spare_array[spare_index+1]<10)
+	{
+		return new Error('Not a spare');
+	}
+	return 10+spare_array[spare_index+2];
+};
+
 const strike = (strike_array,strike_index)=>{
 	if((strike_array.length-strike_index)<3)
 	{
 		return new Error('less number of arguments');
 	}
-	if(strike_array[strike_index]+strike_array[strike_index+1]<10)
+	if(strike_array[strike_index]<10)
 	{
 		return new Error('Not a strike');
 	}
-	return 10+strike_array[strike_index+2];
+	return 10+strike_array[strike_index+1]+strike_array[strike_index+2];
 };
-
 
 // const score = (pins_array)=>{
 
@@ -22,4 +33,4 @@ const strike = (strike_array,strike_index)=>{
 rolls(8,2,6);
 
 
-module.exports = {rolls,strike};
+module.exports = {rolls,spare,strike};
--- assignment-1/bowling.test.js
+++ assignment-1/bowling.test.js
@@ -7,12 +7,26 @@ describe('testing rolls function',()=>{
 		expect(bowlingFunctions.rolls()).toEqual([]);
 	});
 });
-describe('testing strike function',()=>{
+describe('testing spare function',()=>{
 	it('should return 10 ',()=>{
-		expect(bowlingFunctions.strike([9,1,0,6],0)).toEqual(10);
+		expect(bowlingFunctions.spare([9,1,0,6],0)).toEqual(10);
 	});
 	it('should return error: second index is not a strike',()=>{
+		expect(bowlingFunctions.spare([1,2,3],0)).toEqual(new Error('Not a spare'));
+	});
+	it('should return error: less number of arguements',()=>{
+		expect(bowlingFunctions.spare([1,2],0)).toEqual(new Error('less number of arguments'));
+	});
+});
+describe('testing strike function',()=>{
+	it('should return 11 ',()=>{
+		expect(bowlingFunctions.strike([10,1,0,6],0)).toEqual(11);
+	});
+	it('should return error: first index is not a strike',()=>{
 		expect(bowlingFunctions.strike([1,2,3],0)).toEqual(new Error('Not a strike'));
+	});
+	it('should return error: first index is not a strike',()=>{
+		expect(bowlingFunctions.strike([9,1,10],0)).toEqual(new Error('Not a strike'));
 	});
 	it('should return error: less number of arguements',()=>{
 		expect(bowlingFunctions.strike([1,2],0)).toEqual(new Error('less number of arguments'));
