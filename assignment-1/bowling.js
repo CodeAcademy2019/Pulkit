39c12d52ab4eb9ff7a6a4a8495a15af4211f012b
Pulkit Agarwal <pulkit_agarwal-bng@external.mckinsey.com>
2019-01-22 10:47:25 +0530

    strike function added: calculate score on strike

--- assignment-1/bowling.js
+++ assignment-1/bowling.js
@@ -1,15 +1,25 @@
 const rolls = (...arg)=>{
+	console.log(arg);
+	console.log(strike(arg,0));
 	return [...arg];
 };
+const strike = (strike_array,strike_index)=>{
+	if((strike_array.length-strike_index)<3)
+	{
+		return new Error('less number of arguments');
+	}
+	if(strike_array[strike_index]+strike_array[strike_index+1]<10)
+	{
+		return new Error('Not a strike');
+	}
+	return 10+strike_array[strike_index+2];
+};
+
+
 // const score = (pins_array)=>{
 
 // };
-// console.log(rolls(1,2,3));
-// const mainProgram= ()=>
-// {
-// 	const pins_array = [...arg];
+rolls(8,2,6);
 
 
-// };
-
-module.exports = {rolls};
+module.exports = {rolls,strike};
--- assignment-1/bowling.test.js
+++ assignment-1/bowling.test.js
@@ -7,3 +7,14 @@ describe('testing rolls function',()=>{
 		expect(bowlingFunctions.rolls()).toEqual([]);
 	});
 });
+describe('testing strike function',()=>{
+	it('should return 10 ',()=>{
+		expect(bowlingFunctions.strike([9,1,0,6],0)).toEqual(10);
+	});
+	it('should return error: second index is not a strike',()=>{
+		expect(bowlingFunctions.strike([1,2,3],0)).toEqual(new Error('Not a strike'));
+	});
+	it('should return error: less number of arguements',()=>{
+		expect(bowlingFunctions.strike([1,2],0)).toEqual(new Error('less number of arguments'));
+	});
+});
